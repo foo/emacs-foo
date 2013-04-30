bed6b117a2b43b9b294815f7bb462a8808626c39
Maciek Pacut <maciek.pacut@gmail.com>
2013-04-30 11:44:14 +0200

    Escape to emacs state by pressing C-z in any state.

--- evil-states/foo-all-states.el
+++ evil-states/foo-all-states.el
@@ -1,1 +1,9 @@
+(defvar foo-all-mode-maps
+  '(evil-insert-state-map
+    evil-normal-mode-map
+    evil-movement-mode-map))
+
+(dolist (m foo-all-mode-maps)
+  (define-key m "C-z" 'evil-emacs-state))
+
 (provide 'foo-all-states)
--- foo-evil-states.el
+++ foo-evil-states.el
@@ -1,13 +1,9 @@
 (add-to-list 'load-path
 	     (concat foo-config-dir "evil-states"))
 
-(defvar foo-all-states
-  '(evil-insert-state
-    evil-normal-mode
-    evil-movement-mode))
-
 (require 'evil)
 (evil-mode 1)
+(setq evil-set-initial-state 'evil-normal-mode-map)
 
 (require 'foo-normal-state)
 (require 'foo-insert-state)

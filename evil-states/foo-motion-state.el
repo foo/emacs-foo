0afcda812cde4e330f57714d1d8edfad4c192617
Maciek Pacut <maciek.pacut@gmail.com>
2013-04-30 21:03:38 +0200

    Movement made easier.

--- evil-states/foo-motion-state.el
+++ evil-states/foo-motion-state.el
@@ -1,1 +1,16 @@
+; entering other states
+(define-key evil-motion-state-map (kbd "C-n") 'evil-execute-in-normal-state)
+(define-key evil-motion-state-map (kbd "u") 'evil-execute-in-normal-state)
+(define-key evil-motion-state-map (kbd "i") 'evil-insert-state)
+
+; motion
+(define-key evil-motion-state-map "r" 'evil-forward-word-end)
+(define-key evil-motion-state-map "c" 'evil-backward-word-begin)
+(define-key evil-motion-state-map "n" 'evil-forward-char)
+(define-key evil-motion-state-map "d" 'evil-backward-char)
+(define-key evil-motion-state-map "h" 'evil-next-line)
+(define-key evil-motion-state-map "t" 'evil-previous-line)
+
+(define-key evil-motion-state-map "s" 'isearch-forward)
+
 (provide 'foo-motion-state)

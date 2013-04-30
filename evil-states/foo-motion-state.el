b2902a5c921b33d43a7417be6f9abcea4437c146
Maciek Pacut <maciek.pacut@gmail.com>
2013-04-30 21:24:59 +0200

    Scrolling on QWERTY n and p.

--- evil-states/foo-motion-state.el
+++ evil-states/foo-motion-state.el
@@ -6,10 +6,14 @@
 ; motion
 (define-key evil-motion-state-map "r" 'evil-forward-word-end)
 (define-key evil-motion-state-map "c" 'evil-backward-word-begin)
+
 (define-key evil-motion-state-map "n" 'evil-forward-char)
 (define-key evil-motion-state-map "d" 'evil-backward-char)
 (define-key evil-motion-state-map "h" 'evil-next-line)
 (define-key evil-motion-state-map "t" 'evil-previous-line)
+
+(define-key evil-motion-state-map "l" 'evil-scroll-up)
+(define-key evil-motion-state-map "b" 'evil-scroll-down)
 
 (define-key evil-motion-state-map "s" 'isearch-forward)
 

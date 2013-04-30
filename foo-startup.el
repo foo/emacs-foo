f18b44cdcaeae864d84cc9e72b3810b1fe8cc7e7
Maciek Pacut <maciek.pacut@gmail.com>
2013-04-30 11:07:37 +0200

    Add files to load list.

--- foo-startup.el
+++ foo-startup.el
@@ -1,3 +1,5 @@
+(add-to-list 'load-path "~/code/emacs-foo/startup")
+
 (require 'foo-appearance)
 
 (provide 'foo-startup)
--- init.el
+++ init.el
@@ -1,8 +1,13 @@
-; foo- prefix convention
+; temporary placement, as this version
+; of config lacks needed features
+(setq foo-config-dir "~/code/emacs-foo")
 
+(add-to-list 'load-path "~/code/emacs-foo")
+
+; foo- prefix convention
 (require 'foo-startup)
 
-(set foo-all-states
-    ('evil-insert-state
-     'evil-normal-mode
-     'evil-movement-mode))
+(setq foo-all-states
+    '(evil-insert-state
+      evil-normal-mode
+      evil-movement-mode))

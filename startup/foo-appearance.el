747a1f878a1858382a085b3af5932fafe0c60d03
Maciek Pacut <maciek.pacut@gmail.com>
2013-04-30 11:59:14 +0200

    Loose some parts of UI

--- startup/foo-appearance.el
+++ startup/foo-appearance.el
@@ -1,3 +1,13 @@
 (setq inhibit-startup-message t)
 
+;; disable parts of UI
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(tool-bar-mode -1)
+
+;; cursor
+(blink-cursor-mode -1)
+(setq evil-default-cursor t)
+
 (provide 'foo-appearance)

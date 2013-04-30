7526b3adb7d351f15ec37df9e7ee8c204e0028cc
Maciek Pacut <maciek.pacut@gmail.com>
2013-04-30 20:51:06 +0200

    Keybinding to enter motion state.

--- evil-states/foo-insert-state.el
+++ evil-states/foo-insert-state.el
@@ -1,4 +1,4 @@
-(define-key evil-insert-state-map "\C-n" 'evil-execute-in-normal-state)
-;(define-key evil-insert-state-map "\H-m" 'evil-motion-state)
+(define-key evil-insert-state-map (kbd "C-n") 'evil-execute-in-normal-state)
+(define-key evil-insert-state-map (kbd "H-m") 'evil-motion-state)
 
 (provide 'foo-insert-state)

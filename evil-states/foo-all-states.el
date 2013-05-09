3d3cbe8a82a510912bf74dad826a689ec116fbe2
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-09 10:39:22 +0200

    Dired here function displays dired buffer with cursor on the file, from which dired-here was called.

--- evil-states/foo-all-states.el
+++ evil-states/foo-all-states.el
@@ -2,7 +2,11 @@
 
 (defun foo-dired-here ()
   (interactive)
-  (find-file "."))
+  (let ((current-buffer (buffer-file-name)))
+    (find-file ".")
+    (when (stringp buffer)
+	(beginning-of-buffer)
+	(search-forward current-buffer nil nil))))
 
 (define-key evil-normal-state-map "f" 'foo-dired-here)
 (define-key evil-motion-state-map "f" 'foo-dired-here)

b7250cd024aff2d37bfadafa8fb05d2cf67d58a4
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-12 14:29:38 +0200

    Maximize frame after load.

--- startup/foo-appearance.el
+++ startup/foo-appearance.el
@@ -1,34 +1,44 @@
-(setq inhibit-startup-message t)
+(defun foo-appearance-init ()
+  (setq inhibit-startup-message t)
 
-;; disable parts of UI
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tooltip-mode -1)
-(tool-bar-mode -1)
+  ;; disable parts of UI
+  (menu-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1)
+  (tool-bar-mode -1)
 
-;; cursor
-(blink-cursor-mode -1)
-(setq evil-default-cursor t)
+  ;; cursor
+  (blink-cursor-mode -1)
+  (setq evil-default-cursor t)
 
-;; font
-(set-default-font "Ubuntu Mono-13:bold")
+  ;; font
+  (set-default-font "Ubuntu Mono-13:bold")
 
-;; color theme
-(load-theme '
-;adwaita
-;deeper-blue
-;dichromacy
-;leuven
-;light-blue
-;manoj-dark
-misterioso
-;tango
-;tango-dark
-;tsdh-dark
-;tsdh-light
-;wheatgrass
-;whiteboard
-;wombat
-t)
+  ;; color theme
+  (load-theme '
+					;adwaita
+					;deeper-blue
+					;dichromacy
+					;leuven
+					;light-blue
+					;manoj-dark
+   misterioso
+					;tango
+					;tango-dark
+					;tsdh-dark
+					;tsdh-light
+					;wheatgrass
+					;whiteboard
+					;wombat
+   t)
+  
+  (x-send-client-message
+   nil 0 nil "_NET_WM_STATE" 32
+   '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+  (x-send-client-message
+   nil 0 nil "_NET_WM_STATE" 32
+   '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0)))
+
+(add-hook 'after-init-hook 'foo-appearance-init)
 
 (provide 'foo-appearance)

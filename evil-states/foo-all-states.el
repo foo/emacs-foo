74ebf50d9e9a67c090a257935d7f6e54fd70ff40
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-24 18:35:13 +0200

    Rename some states. New mode maps - just for clean keybindings.

--- evil-states/foo-all-states.el
+++
@@ -1,7 +0,0 @@
-(require 'evil)
-
-(define-key evil-normal-state-map "f" 'dired-jump)
-(define-key evil-motion-state-map "f" 'dired-jump)
-(define-key evil-window-state-map "f" 'dired-jump)
-
-(provide 'foo-all-states)

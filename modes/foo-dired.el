bf52273f94dbe2a464e7322b9aa1853f0daf493e
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-24 13:15:38 +0200

    Allow window state enter in dired.

--- modes/foo-dired.el
+++ modes/foo-dired.el
@@ -3,10 +3,12 @@
 (defun foo-dired-keybindings ()
   (require 'dired)
   (require 'evil)
+  (require 'foo-evil-states)
   (evil-define-key 'normal dired-mode-map "r" 'isearch-backward)
   (evil-define-key 'normal dired-mode-map "s" 'isearch-forward)
   (evil-define-key 'normal dired-mode-map "c" 'dired-up-directory)
   (evil-define-key 'normal dired-mode-map "t" 'dired-previous-line)
+  (evil-define-key 'normal dired-mode-map "w" 'evil-window-state)
   (evil-define-key 'normal dired-mode-map "h" 'dired-next-line))
 
 

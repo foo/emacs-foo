a2b92054f17b3d0db95f17ccbb43ddcb1de8052f
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-24 12:48:36 +0200

    Switching between windows with preservation of window-state.

--- evil-states/foo-window-state.el
+++ evil-states/foo-window-state.el
@@ -41,6 +41,19 @@
 (define-key evil-window-state-map "w" 'next-window-with-normal-state)
 (define-key evil-window-state-map "C-w" 'next-window-with-normal-state)
 
+(defun next-window-with-window-state ()
+  (interactive)
+  (other-window 1)
+  (evil-window-state))
+
+(defun prev-window-with-window-state ()
+  (interactive)
+  (other-window -1)
+  (evil-window-state))
+
+(define-key evil-window-state-map "r" 'next-window-with-window-state)
+(define-key evil-window-state-map "c" 'prev-window-with-window-state)
+
 (define-key evil-window-state-map "h" 'delete-other-windows)
 (define-key evil-window-state-map "t" 'split-window-below)
 (define-key evil-window-state-map "n" 'split-window-right)

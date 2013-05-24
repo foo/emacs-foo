f5d85a3a4040ee5d225755d33ee648dbeefb35de
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-24 13:17:25 +0200

    Dired mode enter in window state.

--- evil-states/foo-all-states.el
+++ evil-states/foo-all-states.el
@@ -4,11 +4,12 @@
   (interactive)
   (let ((current-buffer (buffer-file-name)))
     (find-file ".")
-    (when (stringp buffer)
-	(beginning-of-buffer)
-	(search-forward current-buffer nil nil))))
+    (when (stringp current-buffer)
+      (beginning-of-buffer)
+      (search-forward current-buffer nil nil))))
 
 (define-key evil-normal-state-map "f" 'foo-dired-here)
 (define-key evil-motion-state-map "f" 'foo-dired-here)
+(define-key evil-window-state-map "f" 'foo-dired-here)
 
 (provide 'foo-all-states)

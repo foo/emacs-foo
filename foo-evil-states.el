9f2a7404a0c9a4a3503a4c438f37f0be0ef2ef52
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-25 11:21:05 +0200

    Set motion state as default.

--- foo-evil-states.el
+++ foo-evil-states.el
@@ -1,35 +1,25 @@
 (add-to-list 'load-path
 	     (concat foo-config-dir "evil-states"))
 
+(setq evil-default-state 'motion)
+
 (require 'evil)
+(evil-mode 1)
 
 (evil-define-state window
   "Window manipulation state"
   :tag " <W> "
   :message "-- window manipulation --")
 
-(setcdr evil-window-state-map nil) 
-
-(evil-define-state movement
-  "Read only movement state"
-  :tag " <MM> "
-  :message "-- movement --")
-
-(setcdr evil-movement-state-map nil) 
-
 (evil-define-state edit
   "Edit state"
   :tag " <ED> "
   :message "-- edit --")
 
-(setcdr evil-edit-state-map nil) 
 
 (require 'foo-window-state)
 (require 'foo-edit-state)
 (require 'foo-insert-state)
-(require 'foo-movement-state)
-
-(setq evil-set-initial-state 'evil-movement-state)
-(evil-mode 1)
+(require 'foo-motion-state)
 
 (provide 'foo-evil-states)

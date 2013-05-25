5414af93ad7a9ec0bc1d817b72d6018700e9931a
Maciek Pacut <maciek.pacut@gmail.com>
2013-05-25 13:05:12 +0200

    Fixed magit change to window state.

--- modes/foo-magit.el
+++ modes/foo-magit.el
@@ -4,6 +4,7 @@
   (evil-set-initial-state 'magit-status-mode 'emacs)
   (define-key magit-status-mode-map "f" '(lambda () (interactive) (dired ".")))
   (define-key magit-status-mode-map "t" 'magit-goto-next-section)
+  (define-key magit-status-mode-map "w" 'evil-window-state)
   (define-key magit-status-mode-map "n" 'magit-goto-previous-section))
 
 (add-hook 'magit-mode-hook 'foo-magit-configure)
